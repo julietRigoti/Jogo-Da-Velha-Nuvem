70f2b50f77af88b0a0d936e4348434b758f68f60
juliet Rigoti <oliveirarigoti@hotmail.com>
2025-03-15 18:48:25 -0300

    Atualiza script de build no package.json e remove alias desnecessário no vite.config.js

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react-dom/client": "react-dom",
-    },
-  },
   build: {
     outDir: 'dist',
   },
