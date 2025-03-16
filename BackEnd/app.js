ed1547cc3bd9f6af29efe941b484475dcfe01e24
Vitória Melo <95388328+vitoriameloo0@users.noreply.github.com>
2025-03-15 22:43:24 -0300

    salvando mudancas locais

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -17,7 +17,7 @@ const users = require('./controllers/users');
 // Criar as rotas
 app.use('/', users);
 
-// Iniciar o servidor na porta 8080, criar a funcao utilizando modelo Arrow Function para retornar a mensagem de sucesso
-app.listen(8080, () => {
-    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+// Iniciar o servidor na porta 5000, criar a funcao utilizando modelo Arrow Function para retornar a mensagem de sucesso
+app.listen(5000, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:5000");
 });
