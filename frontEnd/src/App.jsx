95a84ac0902b1d562c1da4dc51886c34299fa351
juliet Rigoti <oliveirarigoti@hotmail.com>
2025-03-16 22:02:17 -0300

    atualiza o componente App para refletir as mudanças no GameProvider e ajusta a formatação das rotas

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { GameProvider } from './contexts/GameContext';
+import { GameProvider } from './contexts/GameContext'; // Atualizado para o novo GameProvider
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import CreateRooms from './pages/CreateRoom';
@@ -13,15 +13,14 @@ function App() {
   return (
     <GameProvider>
       <Router>
-        <Routes>  
+        <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/create-room" element={<CreateRooms />} /> 
+          <Route path="/create-room" element={<CreateRooms />} />
           <Route path="/roomlist" element={<RoomList />} />
           <Route path="/join-room/:idSala" element={<JoinRoom />} />
           <Route path="/room/:idSala" element={<Room />} />
           <Route path="*" element={<Home />} />
-         
         </Routes>
       </Router>
     </GameProvider>
